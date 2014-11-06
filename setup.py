6f6c9868f607473b02525dab3e968cc8423d5bd6
Lezzoum Djebran <ldjebran@gmail.com>
2014-11-06 11:29:40 +0200

    use external test module

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
 
 from setuptools import setup, find_packages
 
-import unittest
-
-
-def all_tests():
-    test_loader = unittest.TestLoader()
-    return test_loader.loadTestsFromNames(['twsfolders.tests.test_rootfolders',
-                                           'twsfolders.tests.test_folder'
-                                           ]
-                                          )
 
 setup(name='twsfolders',
-      version='0.1.0',
+      version='0.1.2',
       platforms=['any'],
       packages=find_packages(),
       zip_safe=True,
@@ -21,7 +12,7 @@ setup(name='twsfolders',
       author='Djebran Lezzoum',
       author_email='ldjebran@gmail.com',
       description='TeamWorks Folders',
-      test_suite="__main__.all_tests",
+      test_suite="test.all_tests",
       keywords='ZODDB persistent folders folder item url rootfolders',
       install_requires=['zope.interface', 'persistent', 'ZODB'],
       classifiers=["Programming Language :: Python :: 2.7",
